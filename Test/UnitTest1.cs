e0075a938c3373d606d37b8d47c5cf45444cbf81
Jeff Weiler <jeffweiler8770@gmail.com>
2015-04-22 14:30:57 -0400

    added project

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsInUnitTest()
+        {
+            Assert.IsTrue(UnitTestDetector.IsInUnitTest,
+                "Should detect that we are running inside a unit test."); // lol
+        }
+
+        [TestMethod]
+        public void ShouldReturn()
+        {
+            Program.Main(new string[]{});
+        }
+    }
+}
